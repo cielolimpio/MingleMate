47ecc3b8577801f9a537ee759d33cb547a411c59
cielolimpio <saintkim0724@naver.com>
2023-01-20 18:05:57 +0900

    initialize

---
+++ gongyeon/src/main/java/com/gongyeon/gongyeon/GongyeonApplication.java
@@ -0,0 +1,13 @@
+package com.gongyeon.gongyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongyeonApplication.class, args);
+	}
+
+}
---
+++ gongyeon/src/test/java/com/gongyeon/gongyeon/GongyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongyeon.gongyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

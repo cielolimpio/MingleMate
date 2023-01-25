df90bc108ba6a3fac02a92c4ffef5720fb401d88
cielolimpio <saintkim0724@naver.com>
2023-01-25 23:11:10 +0900

    test initialize

--- gongyeon/src/main/java/com/gongyeon/gongyeon/domain/BaseEntity.java
+++ gongyeon/src/main/java/com/gongyeon/gongyeon/domain/BaseEntity.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -13,8 +14,13 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(name = "registered_datetime")
     private LocalDateTime registeredDateTime;
+
     @LastModifiedDate
+    @Column(name = "last_modified_datetime")
     private LocalDateTime lastModifiedDateTime;
+
+    @Column(name = "deleted_datetime")
     private LocalDateTime deletedDateTime;
 }

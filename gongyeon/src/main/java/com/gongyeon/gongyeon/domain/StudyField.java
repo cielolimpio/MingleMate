43b94d108debf16fe388584dda697842671da8fe
cielolimpio <saintkim0724@naver.com>
2023-01-26 22:50:57 +0900

    study_field and category enum

--- gongyeon/src/main/java/com/gongyeon/gongyeon/domain/StudyField.java
+++ gongyeon/src/main/java/com/gongyeon/gongyeon/domain/StudyField.java
@@ -1,2 +1,37 @@
-package com.gongyeon.gongyeon.domain;public class StudyField {
+package com.gongyeon.gongyeon.domain;
+
+import com.gongyeon.gongyeon.enums.CategoryEnum;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "study_fields")
+@Getter
+public class StudyField extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
+
+
+    public static StudyField createStudyField(Member member, String name, CategoryEnum category) {
+        StudyField studyField = new StudyField();
+        studyField.member = member;
+        studyField.name = name;
+        studyField.category = category;
+
+        return studyField;
+    }
 }

84de76572a8a02d717520a812fff22edaa9d7d62
suudh99 <luke@cheloniacorp.com>
2023-01-31 02:31:46 +0900

    add logout

--- gongyeon/src/main/java/com/gongyeon/gongyeon/controller/MemberController.java
+++ gongyeon/src/main/java/com/gongyeon/gongyeon/controller/MemberController.java
@@ -1,19 +1,17 @@
 package com.gongyeon.gongyeon.controller;
 
-import com.gongyeon.gongyeon.controller.dto.MemberLoginDto;
+import com.gongyeon.gongyeon.payload.request.MemberLoginRequest;
 import com.gongyeon.gongyeon.domain.Member;
-import com.gongyeon.gongyeon.enums.GenderEnum;
 import com.gongyeon.gongyeon.enums.RoleEnum;
 import com.gongyeon.gongyeon.payload.request.SignUpRequest;
 import com.gongyeon.gongyeon.repository.MemberRepository;
-import com.gongyeon.gongyeon.security.TokenInfo;
+import com.gongyeon.gongyeon.models.TokenInfo;
 import com.gongyeon.gongyeon.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @RestController
@@ -21,29 +19,61 @@ import javax.annotation.PostConstruct;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
-    private final MemberRepository memberRepository;
 
-//    @PostMapping("/sign-up")
-//    public ResponseEntity<Long> signUp(
-//            @RequestBody SignUpRequest request
-//    ) {
-//        Member member = Member.createMember(
-//                request.getName(),
-//                request.getEmail(),
-//                request.getPassword(),
-//                request.getGender(),
-//                request.getAge(),
-//                request.getAddress()
-//        );
-//        Long id = memberService.signUp(member);
-//
-//        return ResponseEntity.ok(id);
-//    }
+    @PostMapping("/sign-up")
+    public ResponseEntity<Long> signUp(
+            @RequestBody SignUpRequest request
+    ) {
+        Member member = Member.createMember(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getGender(),
+                request.getAge(),
+                request.getAddress()
+        );
+        member.changeRole(RoleEnum.USER);
+        Long id = memberService.signUp(member);
+
+        return ResponseEntity.ok(id);
+    }
 
     @PostMapping("/login")
-    public TokenInfo login(@RequestBody MemberLoginDto memberLoginDto){
-        log.info("login request");
-        return memberService.login(memberLoginDto.getEmail(), memberLoginDto.getPassword());
+    public TokenInfo login(@RequestBody MemberLoginRequest memberLoginRequest){
+        log.info("Login Request");
+        return memberService.login(memberLoginRequest.getEmail(), memberLoginRequest.getPassword());
     }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(@RequestBody TokenInfo tokenInfo){
+        log.info("Reissue Request");
+        return memberService.reissue(tokenInfo);
+    }
+    
+    
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader(value = "Authorization") String accessTokenWithType){
+        log.info("Logout Request");
+        String accessToken = accessTokenWithType.substring(7);
+        return memberService.logout(accessToken);
+    }
+
+//    @GetMapping("/user")
+//    public String user(){
+//        return "ok";
+//    }
+//
+//    @GetMapping("/admin")
+//    public String admin(){
+//        return "ok";
+//    }
+//
+//    @PostConstruct
+//    public void init(){
+//        Member member = Member.createMember("동현", "test@naver.com", "1234", GenderEnum.MALE, 30, null);
+//        member.changeRole(RoleEnum.USER);
+//        memberRepository.save(member);
+//    }
+
 
 }

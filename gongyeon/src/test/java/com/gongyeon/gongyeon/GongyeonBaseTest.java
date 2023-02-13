2efb6e410fba0a2d303b28eab932d10f459438f4
yumin-tripleable <tripleable.yumin@gmail.com>
2023-02-13 15:20:06 +0900

    signup, login, reissue, logout test

--- gongyeon/src/test/java/com/gongyeon/gongyeon/GongyeonBaseTest.java
+++ gongyeon/src/test/java/com/gongyeon/gongyeon/GongyeonBaseTest.java
@@ -11,4 +11,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Disabled
 @AutoConfigureMockMvc
 @Transactional
-public class GongyeonBaseTest {}
+public class GongyeonBaseTest extends GongyeonTestHelper {}
--- gongyeon/src/test/java/com/gongyeon/gongyeon/service/MemberServiceTest.java
+++ gongyeon/src/test/java/com/gongyeon/gongyeon/service/MemberServiceTest.java
@@ -2,13 +2,21 @@ package com.gongyeon.gongyeon.service;
 
 import com.gongyeon.gongyeon.GongyeonBaseTest;
 import com.gongyeon.gongyeon.domain.Member;
+import com.gongyeon.gongyeon.domain.RefreshToken;
+import com.gongyeon.gongyeon.domain.StudyField;
+import com.gongyeon.gongyeon.domain.embeddedTypes.Address;
+import com.gongyeon.gongyeon.domain.embeddedTypes.DaysOfTheWeek;
+import com.gongyeon.gongyeon.enums.CategoryEnum;
+import com.gongyeon.gongyeon.enums.GenderEnum;
 import com.gongyeon.gongyeon.models.TokenInfo;
 import com.gongyeon.gongyeon.repository.MemberRepository;
+import com.gongyeon.gongyeon.repository.RefreshTokenRepository;
 import com.gongyeon.gongyeon.security.JwtTokenProvider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -19,8 +27,11 @@ public class MemberServiceTest extends GongyeonBaseTest {
     @Autowired
     MemberRepository memberRepository;
     @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+    @Autowired
     JwtTokenProvider jwtTokenProvider;
 
+    //signUp
     @Test
     public void signUp() throws Exception {
         //given
@@ -49,5 +60,95 @@ public class MemberServiceTest extends GongyeonBaseTest {
         Assertions.assertThatThrownBy(() -> {
             memberService.signUp(member2);
         });
+    }
+
+    //login
+    @Test
+    public void 로그인() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        memberService.signUp(member);
+        //when
+        TokenInfo tokenInfo = memberService.login(member.getEmail(), member.getPassword());
+        //then
+        String email = jwtTokenProvider.getAuthentication(tokenInfo.getAccessToken()).getName();
+        Assertions.assertThat(email).isEqualTo(member.getEmail());
+    }
+
+    @Test
+    public void 로그인_시_이메일_없음() throws Exception {
+        Assertions.assertThatThrownBy(() -> {
+            memberService.login("test1@gmail.com", "test1");
+        });
+    }
+
+    //reissue
+    @Test
+    public void reissue() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        TokenInfo tokenInfo = memberService.signUp(member);
+        //when
+        TokenInfo newTokenInfo = memberService.reissue(tokenInfo);
+        Authentication authentication = jwtTokenProvider.getAuthentication(newTokenInfo.getAccessToken());
+        //then
+        Assertions.assertThat(refreshTokenRepository.findById(authentication.getName())).isPresent();
+        Assertions.assertThat(newTokenInfo.getRefreshToken()).isNotEqualTo(tokenInfo.getRefreshToken());
+        Assertions.assertThat(jwtTokenProvider.getAuthentication(newTokenInfo.getAccessToken()).getName())
+                .isEqualTo(member.getEmail());
+    }
+
+    @Test
+    public void reissue_시_잘못된_access_token() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        TokenInfo tokenInfo = memberService.signUp(member);
+        //when
+        tokenInfo.setAccessToken("wrong_access_token");
+        //then
+        Assertions.assertThatThrownBy(() -> {
+            memberService.reissue(tokenInfo);
+        });
+    }
+
+    @Test
+    public void reissue_시_잘못된_refresh_token() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        TokenInfo tokenInfo = memberService.signUp(member);
+        //when
+        tokenInfo.setRefreshToken("wrong_refresh_token");
+        //then
+        Assertions.assertThatThrownBy(() -> {
+            memberService.reissue(tokenInfo);
+        });
+    }
+
+    //logout
+    @Test
+    public void logout() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        TokenInfo tokenInfo = memberService.signUp(member);
+        Authentication authentication = jwtTokenProvider.getAuthentication(tokenInfo.getAccessToken());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //when
+        memberService.logout();
+        //then
+        Assertions.assertThat(refreshTokenRepository.findById(member.getEmail())).isNotPresent();
+    }
+
+    @Test
+    public void logout_시_redis에_refresh_token_없음() throws Exception {
+        //given
+        Member member = Member.createMember("test1", "test1@gmail.com", "test1");
+        TokenInfo tokenInfo = memberService.signUp(member);
+        Authentication authentication = jwtTokenProvider.getAuthentication(tokenInfo.getAccessToken());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //when
+        RefreshToken refreshToken = new RefreshToken(member.getEmail(), tokenInfo.getRefreshToken());
+        refreshTokenRepository.delete(refreshToken);
+        //then
+        Assertions.assertThatThrownBy(() -> memberService.logout());
     }
 }
